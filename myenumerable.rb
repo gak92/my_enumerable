cb93c0fb311b29157f0323edf959444651b2454d
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-22 14:06:20 +0500

    Add MyEnumerable structure in myenumerable file

---
+++ myenumerable.rb
@@ -0,0 +1,17 @@
+module MyEnumerable
+  def all
+
+  end
+
+  def any
+
+  end
+
+  def filter
+
+  end
+
+  def display
+    puts @list
+  end
+end
--- mylist.rb
+++ mylist.rb
@@ -1,9 +1,17 @@
+require_relative 'myenumerable'
+
 class MyList
-  def  initialize(list)
+  def  initialize(*list)
     @list = list
   end
 
-  def each do
-    @list.each { |e| yield e if block_given? }
-  end
+  include MyEnumerable
+
+  # def each do
+  #   @list.each { |e| yield e if block_given? }
+  # end
+
 end
+
+list = MyList.new(1, 2, 3, 4)
+puts list.display
